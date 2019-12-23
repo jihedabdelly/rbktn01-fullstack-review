8a66820c95e6471a31d3f4dff9f9bbaddb86e1da
jihedabdelly <jihednew7@gmail.com>
2019-12-23 20:05:19 +0100

    solo sprint 1

--- database/index.js
+++ database/index.js
@@ -1,16 +1,27 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
+//the nested schema inside repoSchema
+let ownerSchema = mongoose.Schema({
+  url: String,
+  id: Number
+})
+
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  name: String,
+  owner : { type: Schema.ObjectId, ref: 'ownerSchema'}
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (err, user) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  if(err) throw new Error("Error !!!");
+
 }
 
 module.exports.save = save;
